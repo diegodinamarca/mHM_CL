00cdc25139cce840e02900b59a36ae954e746412
mHM <mhm@macminiserver.local>
2025-06-13 09:20:03 -0400

    added processing of meteo forcings

--- R/postprocess_workflow.R
+++ R/postprocess_workflow.R
@@ -11,10 +11,16 @@ source("scripts/r/utils.r")
 list.files("scripts/r/postprocess", full.names = TRUE)
 
 # 1.set the path for the domain where all outputs will be written
-domain_path = "/Users/mhm/Desktop/FONDECYT_CAMILA/mhm_snow/domain_zone_1"
+domain_path = "/Users/mhm/Desktop/FONDECYT_CAMILA/mhm_snow/domain_zone_2"
 # The configuration file must be inside the domain_path
 config_path <- file.path(domain_path, "preprocess_config.json")
 config = read_json(config_path)
+
+for (i in 6:7){
+  domain_path = paste0("/Users/mhm/Desktop/FONDECYT_CAMILA/mhm_snow/domain_zone_",i)
+  process_meteo_variable(domain_path = domain_path, var_name = "pre", roi_mask = TRUE)
+  process_meteo_variable(domain_path = domain_path, var_name = "pet", roi_mask = TRUE)
+}
 
 # visualizar todos los outputs y forcings de un domain
 visualize_annual_outputs(domain_path, mask_roi = TRUE)
@@ -37,7 +43,7 @@ basin = "/Users/mhm/Desktop/FONDECYT_CAMILA/mhm_snow/DATA/SHP/cau_basin.geojson"
 visualize_mosaic_outputs("domain_chile/OUT", roi_file = basin)
 
 # extract single variable from mhm output
-df.q = extract_roi_timeseries("domain_zone_4/OUT/mHM_Fluxes_States.nc", var_name = "Q", roi_file = basin)
+df.q = extract_roi_timeseries("domain_zone_4/OUT/mHM_Fluxes_States.nc", +var_name = "Q", roi_file = basin)
 # extract all outputs from mhm output
 df = extract_roi_timeseries_all("domain_zone_4/OUT/mHM_Fluxes_States.nc", roi_file = basin)
 
